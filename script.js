8f5103ef0689a75c4017ddae313289fba99bc8c4
Roger Pettett <rmp@psyphi.net>
2023-06-25 16:23:45 +0100

    filter by topics now working

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const fillTemplate = (templateString, templateVars) => {
   return new Function("return `"+templateString +"`;").call(templateVars);
 }
 
-const articlesByTag = {}; // a very basic search index
+const articlesByTopic = {}; // a very basic search index
 
 (() => {
+    // show/hide articles based on topic query in URL
+    const topicQueryEncoded = document.location.href // from the URL in the address bar...
+	  .split('#')[0]                    // trim any following internal # anchors
+	  .split('?')[1]                    // trim any preceeding URL
+    ?.split(/[;&]/)                         // split query parameters by ; or &
+    ?.filter(x => x.match(/^topic=/))[0]      // discard anything except topic=xxx
+    ?.split('=')[1];                        // split on = and retain value
+    const topicQuery = decodeURI(topicQueryEncoded);
+
     // load all the article data
     fetch("articles.json")
 	.then(res => { return res.json(); })
 	.then(allArticleData => {
 
-	    // keep track of the article ids (sequentially) so we can reference them later when searching for tags
+	    // keep track of the article ids (sequentially) so we can reference them later when searching for topics
 	    let articleId = 0;
 
 	    // iterate over articles and fill in a copy of the template for each one
@@ -30,18 +39,27 @@ const articlesByTag = {}; // a very basic search index
 			.then(panel => {
 			    // "append" function tacks the panel content onto the end of the article container
 			    $('#articles').append(panel);
+
+			    if(topicQuery) {
+				if(articleData.topics.includes(topicQuery)) {
+				    $(panel).show();
+				} else {
+				    $(panel).hide();
+				}
+			    }
+
 			});
 
 		    if(articleData.topics) {
-			// if there are tags for this article, keep track of them
-			articleData.topics.forEach((tag) => {
-			    if(!articlesByTag[tag]) {
-				// if we haven't stored any articles for this tag, initialise with an empty array
-				articlesByTag[tag] = [];
+			// if there are topics for this article, keep track of them
+			articleData.topics.forEach((topic) => {
+			    if(!articlesByTopic[topic]) {
+				// if we haven't stored any articles for this topic, initialise with an empty array
+				articlesByTopic[topic] = [];
 			    }
 
-			    // store the article id against this tag (meaning we can look up the article ids for any given tag when searching)
-			    articlesByTag[tag].push(articleId);
+			    // store the article id against this topic (meaning we can look up the article ids for any given topic when searching)
+			    articlesByTopic[topic].push(articleId);
 			});
 		    }
 
@@ -50,22 +68,9 @@ const articlesByTag = {}; // a very basic search index
 	})
 
 	.then(() => {
-	    console.log(articlesByTag);
-	    Object.keys(articlesByTag).sort().forEach((tag) => {
-		$('#tags').append($(`<li class="nav-item"><a class="nav-link" href="?tag=${tag}">${tag}</a></li>`));
-	    });
-
-	    const tagQuery = document.location.href   // from the URL in the address bar...
-		  .split('#')[0]                    // trim any following internal # anchors
-		  .split('?')[1]                    // trim any preceeding URL
-	    ?.split(/[;&]/)                         // split query parameters by ; or &
-	    ?.filter(x => x.match(/^tag=/))[0]   // discard anything except tag=xxx
-	    ?.split('=')[1];                     // split on = and retain value
-
-	    const expandArticles = tagQuery ? articlesByTag[decodeURI(tagQuery)] : [ 0 ];
-
-	    expandArticles?.forEach((articleId) => {
-		$(`#collapse${articleId}`).addClass('show');
+	    // populate header bar with topic links
+	    Object.keys(articlesByTopic).sort().forEach((topic) => {
+		$('#topics').append($(`<li class="nav-item"><a class="nav-link" href="?topic=${topic}">${topic}</a></li>`));
 	    });
 	})
 	.then(() => {
